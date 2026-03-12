149c1b81543212b82bac0def15c940cac2a98693
Victor Duforest <vduforest01@gmail.com>
2026-03-12 10:58:41 +0100

    Ajout de la Barre de navigation fonctionnelle

--- app/src/main/java/com/example/mysportcomplexapp/ui/app/App.kt
+++ app/src/main/java/com/example/mysportcomplexapp/ui/app/App.kt
@@ -1,6 +1,11 @@
 package com.example.mysportcomplexapp.ui.app
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
@@ -8,10 +13,16 @@ import androidx.compose.material3.*
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.getValue
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 
 import com.example.mysportcomplexapp.ui.app.screens.AccueilScreen
@@ -20,7 +31,8 @@ import com.example.mysportcomplexapp.ui.app.screens.ResaScreen
 import com.example.mysportcomplexapp.ui.app.screens.ClubScreen
 import com.example.mysportcomplexapp.ui.app.screens.PlanningScreen
 import com.example.mysportcomplexapp.ui.app.screens.ProfilScreen
-
+import com.example.mysportcomplexapp.ui.app.theme.BlueIndicator
+import com.example.mysportcomplexapp.ui.app.theme.Transparent
 @Composable
 fun App(){
     val navController = rememberNavController()
@@ -37,6 +49,9 @@ fun App(){
 @Composable
 fun BottomBar(navController: NavController) {
 
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
     val items = listOf(
         BottomNavItem.Home,
         BottomNavItem.Club,
@@ -48,10 +63,31 @@ fun BottomBar(navController: NavController) {
 
         items.forEach { item ->
 
+            val isSelected = item.route == currentRoute
+
             NavigationBarItem(
-                icon = { Icon(item.icon, contentDescription = item.label) },
+                icon = {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+
+                        Box(
+                            modifier = Modifier
+                                .height(4.dp)
+                                .width(24.dp)
+                                .background(
+                                    color = if (isSelected) BlueIndicator.copy(alpha = 0.3f)
+                                    else Transparent,
+                                    shape = RoundedCornerShape(2.dp)
+                                )
+                        )
+
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Icon(item.icon, contentDescription = item.label)
+                    }
+                       },
                 label = { Text(item.label) },
-                selected = false,
+                selected = isSelected,
                 onClick = {
                     navController.navigate(item.route)
                 }
