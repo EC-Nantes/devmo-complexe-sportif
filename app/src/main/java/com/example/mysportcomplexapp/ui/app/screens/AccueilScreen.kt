f2ce36d66a372cd14c050038413131eb8f6750d9
Victor Duforest <vduforest01@gmail.com>
2026-03-14 13:11:54 +0100

    La page d'accueil est faite

--- app/src/main/java/com/example/mysportcomplexapp/ui/app/screens/AccueilScreen.kt
+++ app/src/main/java/com/example/mysportcomplexapp/ui/app/screens/AccueilScreen.kt
@@ -12,13 +12,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.mysportcomplexapp.R
 import com.example.mysportcomplexapp.ui.app.theme.TennisColor
 import com.example.mysportcomplexapp.ui.app.theme.FootballColor
@@ -28,39 +32,57 @@ import com.example.mysportcomplexapp.ui.app.theme.GymColor
 import com.example.mysportcomplexapp.ui.app.theme.EscaladeColor
 
 import com.example.mysportcomplexapp.ui.app.Sport
+import com.example.mysportcomplexapp.ui.app.theme.MySportComplexAppTheme
+
 @Composable
-fun test(navController: NavHostController) {
+fun AccueilScreen(navController: NavHostController) {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
-    ){
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.fillMaxWidth()
-        ){
-            Image(
-                painter = painterResource(id = R.drawable.complexe),
-                contentDescription = "Image Complexe",
-                modifier = Modifier.size(64.dp),
-                contentScale = ContentScale.Crop
-            )
-            Spacer(modifier = Modifier.width(16.dp))
-            Text(
-                text = "Complexe Sportif Victor Delacôte",
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold
-            )
-        }
+    ){Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(170.dp)
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.complexe),
+            contentDescription = "Image complexe",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+
+        Text(
+            text = "Complexe Sportif\nVictor Delacôte",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            color = Color.White,
+            style = TextStyle(
+                drawStyle = Stroke(width = 15f)
+            ),
+            modifier = Modifier.align(Alignment.Center)
+        )
+
+        Text(
+            text = "Complexe Sportif\nVictor Delacôte",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            color = Color.Black,
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
         Spacer(modifier = Modifier.height(24.dp))
 
         val sports = listOf(
-            Sport("Tennis", R.drawable.tennis, "tennis", TennisColor),
-            Sport("Football", R.drawable.foot, "football", FootballColor),
-            Sport("Padel", R.drawable.padel, "padel", PadelColor),
-            Sport("Musculation", R.drawable.muscu, "muscu", MuscuColor),
-            Sport("Gymnase", R.drawable.gymnase, "gym", GymColor),
-            Sport("Escalade", R.drawable.escalade, "escalade", EscaladeColor)
+            Sport("TENNIS", R.drawable.tennis, "tennis", TennisColor),
+            Sport("FOOT A 5", R.drawable.foot, "football", FootballColor),
+            Sport("PADEL", R.drawable.padel, "padel", PadelColor),
+            Sport("MUSCU", R.drawable.muscu, "muscu", MuscuColor),
+            Sport("GYMNASE", R.drawable.gymnase, "gym", GymColor),
+            Sport("ESCALADE", R.drawable.escalade, "escalade", EscaladeColor)
         )
 
         Column(
@@ -84,7 +106,7 @@ fun test(navController: NavHostController) {
 }
 
 @Composable
-fun AccueilScreen(navController: NavHostController) {
+fun test(navController: NavHostController) {
 
     Box(
         modifier = Modifier.fillMaxSize(),
@@ -109,38 +131,54 @@ fun SportCard(
             .height(80.dp)
             .clickable { onClick() },
         shape = RoundedCornerShape(12.dp),
-        border = BorderStroke(2.dp, color.copy(alpha = 0.9f)),
+        border = BorderStroke(4.dp, color.copy(alpha = 0.9f)),
         colors = CardDefaults.cardColors(
             containerColor = color.copy(alpha = 0.15f)
         )
     ) {
-        Box(modifier = Modifier.fillMaxSize().padding(horizontal = 50.dp)){
-            Text(
-                text = name,
-                fontSize = 40.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.align(Alignment.Center)
-            )
+        Row(verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 40.dp)) {
+            Box(modifier = Modifier.weight(1f), contentAlignment = Alignment.Center){
+                Text(
+                    text = name,
+                    fontSize = 40.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.align(Alignment.Center)
+                )
 
-            Image(
-                painter = painterResource(id = imageRes),
-                contentDescription = "$name image",
-                modifier = Modifier
-                    .size(48.dp)
-                    .align(Alignment.CenterEnd),
-                contentScale = ContentScale.Crop
-            )
+                Image(
+                    painter = painterResource(id = imageRes),
+                    contentDescription = "$name image",
+                    modifier = Modifier
+                        .size(48.dp)
+                        .align(Alignment.CenterEnd),
+                    contentScale = ContentScale.Crop
+                )
+            }
         }
+
     }
 }
 
-@Preview(showBackground = true)
 @Composable
 fun SportCardPreview() {
     SportCard(
-        name = "Tennis",
+        name = "TENNIS",
         imageRes = R.drawable.tennis,
         color = TennisColor,
         onClick = {}
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AccueilScreenPreview() {
+
+    val navController = rememberNavController()
+
+    MySportComplexAppTheme {
+        AccueilScreen(navController)
+    }
 }
